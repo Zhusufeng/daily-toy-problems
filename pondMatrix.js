baeddd2b70cbaca8c2ffbac4874a3c403259b6e6
Lisa Gee <zhusufeng@gmail.com>
2017-10-12 20:03:07 -0700

    Update brainstorm with further ideas pondMatrix

--- pondMatrix.js
+++ pondMatrix.js
@@ -17,15 +17,27 @@ O: Number
 I: Matrix of 0s and 1s
 C:
   Time: Linear
-  Space: Constant
+  Space: Constant? / Linear
 E:
   INPUT                         OUTPUT
   ------------------------------------
   []                              0
   [[ 0, 0, 0, 0]]                 0
   [[ 0, 1, 1, 0]]                 1
+  [[0, 1],
+   [0, 1]]                        1
+  [[1, 0],
+   [0, 1]]                        2
   [[ 1, 1, 1, 1]]                 1
   [[ 1, 0, 1, 0]]                 2
+  [[ 0, 1, 0, 0],
+   [ 0, 1, 0, 0],
+   [ 0, 1, 0, 0],
+   [ 0, 1, 0, 0]]                 1
+  [[ 0, 1, 0, 0],
+   [ 0, 1, 0, 0],
+   [ 0, 1, 1, 1],
+   [ 0, 1, 0, 0]]                 1
   [[ 0, 1, 1, 0],
    [ 1, 0, 0, 0],
    [ 0, 0, 1, 1],
@@ -38,6 +50,20 @@ E:
 
 BRAINSTORM
 ----------
+Think of N-Queens algorithm. Toggle on/off to show if water element has been accounted for
+
+
+Idea #1
+Make empty object called waterAccountedFor
+Use waterAccountFor when you visit a water square and count it towards pond count
+
+Iterate through matrix
+  Go to each element (which is an array)
+  Iterate through each element in that array
+    If element is 0, ignore?
+    If element is 1,
+      Add matrix[i][j] as key with the
+
 
 
 PSEUDOCODE
