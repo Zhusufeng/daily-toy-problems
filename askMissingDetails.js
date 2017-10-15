423f928fa6a56152e59ab9790e86c23d18ca9fac
Lisa Gee <zhusufeng@gmail.com>
2017-10-14 23:47:15 -0700

    Finish general plan, write code, test askMissDet

--- askMissingDetails.js
+++ askMissingDetails.js
@@ -24,8 +24,17 @@ Hi, could you please provide your <property name>.
 - The input array will always be valid and formatted as in the example above.
 */
 
-var getMissingDetails = function() {
+function askForMissingDetails(list) {
+  var answer = list.filter(function(person) {
+    for (var key in person) {
+      if (!person[key]) {
+        person['question'] = 'Hi, could you please provide your ' + key + '.';
+        return person;
+      }
+    }
+  });
 
+  return answer;
 };
 
 /*
@@ -51,3 +60,20 @@ E:
   If there is the 1 null value
   Add question as key to the object
 */
+
+// Test
+console.log(
+  askForMissingDetails( [
+    {fn: null},
+    {country: 'Argentina'},
+    {age: null}
+  ])
+);
+
+console.log(
+  askForMissingDetails( [
+    { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
+  ])
+);
