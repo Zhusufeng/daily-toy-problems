6bd00ebebabbccdd36bd69077eb64e0dcd594473
Lisa Gee <zhusufeng@gmail.com>
2017-10-14 23:41:41 -0700

    Add new prompt and OICE

---
+++ askMissingDetails.js
@@ -0,0 +1,53 @@
+/*
+var list1 = [
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
+];
+
+adds the question property to each object in the input array where the developer has not provided the relevant details (marked with a null value). The value of the question property should be the following string:
+
+Hi, could you please provide your <property name>.
+
+ and returns only the developers with missing details:
+
+ [
+  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+  question: 'Hi, could you please provide your firstName.' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null,
+  question: 'Hi, could you please provide your language.' }
+]
+
+- At most only one of the values will be null.
+- Preserve the order of the original list.
+- Return an empty array [] if there is no developer with missing details.
+- The input array will always be valid and formatted as in the example above.
+*/
+
+var getMissingDetails = function() {
+
+};
+
+/*
+O: Array of objects
+I: Array of objects
+C: Linear
+E:
+  Input                                      Output
+  --------------------------------------------------
+  []                                         []
+  [                                          [
+    {fn: null},                                {fn: null, question: 'Hi, could you please provide your fn.'},
+    {country: 'Argentina'},                    {age: null, question: 'Hi, could you please provide your age.'}
+    {age: null}                              ]
+  ]
+
+  General Plan/Brainstorm
+  -----------------------
+  Take in list
+  Create answer
+  Make answer equal to filter the list
+  Iterate through object
+  If there is the 1 null value
+  Add question as key to the object
+*/
