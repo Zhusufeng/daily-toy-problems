2bb6c49df3bbc84f5eca39aa7167752f879a4b55
Lisa Gee <zhusufeng@gmail.com>
2017-10-30 09:56:37 -0700

    Sort by language first in sortByLanguage

--- sortByLanguage.js
+++ sortByLanguage.js
@@ -22,3 +22,34 @@ Notes:
 The input array will always be valid and formatted as in the example above.
 The array does not include developers coding in the same language and sharing the same name
 */
+/*
+O: Array of objects
+I: Array of objects
+C: Linear
+E:
+
+General Plan:
+Use sort and sort by language
+
+*/
+
+function sortByLanguage(list) {
+  return list.sort(function(dev1, dev2) {
+    if (dev1.language > dev2.language) {
+      return 1;
+
+    } else {
+      return -1;
+    }
+  });
+
+}
+
+// Test
+var list1 = [
+  { firstName: 'Nikau', lastName: 'R.', country: 'New Zealand', continent: 'Oceania', age: 39, language: 'Ruby' },
+  { firstName: 'Precious', lastName: 'G.', country: 'South Africa', continent: 'Africa', age: 22, language: 'JavaScript' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 30, language: 'C' },
+  { firstName: 'Agustin', lastName: 'V.', country: 'Uruguay', continent: 'Americas', age: 19, language: 'JavaScript' }
+];
+console.log(sortByLanguage(list1));
