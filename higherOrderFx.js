ced0d46bc8171b0e6c73b25c463c6173e1593140
Lisa Gee <zhusufeng@gmail.com>
2017-11-10 08:18:47 -0800

    Add map function in higherOrderFx

--- higherOrderFx.js
+++ higherOrderFx.js
@@ -1,4 +1,4 @@
-/* forEach */
+/* Traversing an array */
 
 const forLoopFunc = () => {
   var names = ['Abby', 'Ben', 'Carl', 'Dan', 'Edmund'];
@@ -18,4 +18,64 @@ const forEachFunc = () => {
   });
 };
 
-console.log(forEachFunc());
+// console.log(forEachFunc());
+
+/* Projecting Arrays */
+function projectArrays () {
+  var newReleases = [
+      {
+        "id": 70111470,
+        "title": "Die Hard",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+      },
+      {
+        "id": 654356453,
+        "title": "Bad Boys",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{ id: 432534, time: 65876586 }]
+      },
+      {
+        "id": 65432445,
+        "title": "The Chamber",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [4.0],
+        "bookmark": []
+      },
+      {
+        "id": 675465,
+        "title": "Fracture",
+        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+        "rating": [5.0],
+        "bookmark": [{ id: 432534, time: 65876586 }]
+      }
+    ],
+    videoAndTitlePairs = [];
+
+  newReleases.forEach(movie => {
+    videoAndTitlePairs.push({id: movie.id, title: movie.title});
+  });
+
+  return videoAndTitlePairs;
+}
+
+// console.log(projectArrays());
+
+// Write the map function
+var myMap = function(array, cb) {
+  var newArray = [];
+
+  array.forEach(item => {
+    newArray.push(cb(item));
+  });
+
+  return newArray;
+};
+
+console.log(myMap([1, 2, 3, 4], function (x) { return x + 1; }));
