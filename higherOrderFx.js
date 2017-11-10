9fbda39cbfcc83e4b2bcbf8315a4b40ee5507436
Lisa Gee <zhusufeng@gmail.com>
2017-11-10 08:31:05 -0800

    Add filtering exercises to higherOrderFx

--- higherOrderFx.js
+++ higherOrderFx.js
@@ -78,4 +78,158 @@ var myMap = function(array, cb) {
   return newArray;
 };
 
-console.log(myMap([1, 2, 3, 4], function (x) { return x + 1; }));
+// console.log(myMap([1, 2, 3, 4], function (x) { return x + 1; }));
+
+function mapNewReleases() {
+  var newReleases = [
+    {
+      "id": 70111470,
+      "title": "Die Hard",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [4.0],
+      "bookmark": []
+    },
+    {
+      "id": 654356453,
+      "title": "Bad Boys",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [5.0],
+      "bookmark": [{ id: 432534, time: 65876586 }]
+    },
+    {
+      "id": 65432445,
+      "title": "The Chamber",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [4.0],
+      "bookmark": []
+    },
+    {
+      "id": 675465,
+      "title": "Fracture",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [5.0],
+      "bookmark": [{ id: 432534, time: 65876586 }]
+    }
+  ];
+
+  return newReleases.map(movie => {
+    return { id: movie.id, title: movie.title };
+  });
+}
+
+// console.log(mapNewReleases());
+
+function forEachFilter() {
+  var newReleases = [
+    {
+      "id": 70111470,
+      "title": "Die Hard",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": 4.0,
+      "bookmark": []
+    },
+    {
+      "id": 654356453,
+      "title": "Bad Boys",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": 5.0,
+      "bookmark": [{ id: 432534, time: 65876586 }]
+    },
+    {
+      "id": 65432445,
+      "title": "The Chamber",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": 4.0,
+      "bookmark": []
+    },
+    {
+      "id": 675465,
+      "title": "Fracture",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": 5.0,
+      "bookmark": [{ id: 432534, time: 65876586 }]
+    }
+  ];
+  var videos = [];
+
+  newReleases.forEach(movie => {
+    if (movie.rating >= 5) {
+      videos.push(movie);
+    }
+  });
+
+  return videos;
+}
+
+// console.log(forEachFilter());
+
+var myFilter = function(array, predicate) {
+  var newArray = [];
+
+  array.forEach(item => {
+    if (predicate(item)){
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
+}
+
+// console.log(myFilter([1, 2, 3, 4], function(x) { return x % 2 === 0}));
+
+function findHighRatingTitle () {
+  var newReleases = [
+    {
+      "id": 70111470,
+      "title": "Die Hard",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": 4.0,
+      "bookmark": []
+    },
+    {
+      "id": 654356453,
+      "title": "Bad Boys",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": 5.0,
+      "bookmark": [{ id: 432534, time: 65876586 }]
+    },
+    {
+      "id": 65432445,
+      "title": "The Chamber",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": 4.0,
+      "bookmark": []
+    },
+    {
+      "id": 675465,
+      "title": "Fracture",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": 5.0,
+      "bookmark": [{ id: 432534, time: 65876586 }]
+    }
+  ];
+
+  return newReleases
+    .filter(movie => {
+      if (movie.rating >= 5) {
+        return movie;
+      }
+    })
+    .map(movie => {
+      return movie.id;
+    });
+}
+
+console.log(findHighRatingTitle());
