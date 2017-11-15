f5e4c2f8110a5fe345c851d7b59e1b0c1c36f533
Lisa Gee <zhusufeng@gmail.com>
2017-11-15 08:30:56 -0800

    Add largestBoxart by forEach to higherOrderFx

--- higherOrderFx.js
+++ higherOrderFx.js
@@ -543,4 +543,29 @@ function pracConcatMap() {
     })
   }
 
-console.log(pracConcatMap());
+// console.log(pracConcatMap());
+
+// Exercise 15: Use forEach to find the largest box art
+function largestBoxart() {
+  var boxarts = [
+    { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+    { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+    { width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+    { width: 425, height: 150, url: "http://cdn-0.nflximg.com/images/2891/Fracture425.jpg" }
+  ];
+
+  let largestSize = 0;
+  let largestArt = {};
+
+  boxarts.forEach(art => {
+    let size = art.width * art.height;
+    if (size >= largestSize) {
+      largestSize = size;
+      largestArt = art;
+    }
+  });
+
+  return largestArt;
+}
+
+console.log(largestBoxart());
