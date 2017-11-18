477eb7e630ab130302c3d7c369a9d2d3f80a4e05
Lisa Gee <zhusufeng@gmail.com>
2017-11-18 11:36:26 -0800

    Add Array.zip to higherOrderFx

--- higherOrderFx.js
+++ higherOrderFx.js
@@ -821,4 +821,18 @@ function zipWithForLoop() {
   return videoIdAndBookmarkIdPairs;
 }
 
-console.log(zipWithForLoop());
+// console.log(zipWithForLoop());
+
+// Exercise 22: Implement zip
+Array.zip = function(left, right, combiner) {
+  var counter,
+    results = [];
+
+  for(counter = 0; counter < Math.min(left.length, right.length); counter++) {
+    results.push(combiner(left[counter], right[counter]));
+  }
+
+  return results;
+}
+
+console.log(JSON.stringify(Array.zip([1,2,3],[4,5,6], function(left, right) { return left + right }))); // === '[5,7,9]'
