7b20c99da9b546c461bdf92b01c45b7a579ad284
Lisa Gee <zhusufeng@gmail.com>
2017-11-21 15:00:15 -0800

    Understand OICE for capitalize

--- capitalize.js
+++ capitalize.js
@@ -1,3 +1,18 @@
 function capitalize(s,arr){
   return "";
 };
+
+/*
+O: string
+I: string + array of nums
+C: linear
+E:
+'abcdef', [1, 2, 5] => 'aBCdeF'
+'abcdef', [1, 2, 5, 100] => 'aBCdeF'
+'aaa', [0, 0, 0] => 'Aaa'
+'AAA', [0, 0, 0] => 'AAA'
+'aaa', [2] => 'aaA'
+'aaa', [100, 200, 300] => 'aaa'
+'aaa', [5000, 100, 1] => 'aAa'
+
+*/
