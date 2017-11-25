3034545da5706df24df67e6c1e38f64ae1eef16f
Lisa Gee <zhusufeng@gmail.com>
2017-11-24 19:00:52 -0800

    Add note that solution is incomplete, merTwoBinTrees

--- mergeTwoBinaryTrees.js
+++ mergeTwoBinaryTrees.js
@@ -1,3 +1,4 @@
+// Incomplete - it does not traverse both trees to the bottom...
 /*
 Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
 
