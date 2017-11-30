0e97111a1775590b1535d58cec3f29b7f68e82ff
Lisa Gee <zhusufeng@gmail.com>
2017-11-30 08:52:04 -0800

    Add buildOneTwoThree for llpushAndBuild

--- llpushAndBuild.js
+++ llpushAndBuild.js
@@ -33,17 +33,24 @@ function push(head, data) {
 }
 
 function buildOneTwoThree() {
-  // Go.
+  var head = null;
+  head = push(head, 3);
+  head = push(head, 2);
+  head = push(head, 1);
+
+  return head;
 }
 
 // Test
-var chained = null
-console.log('chained ==> ', chained);
-chained = push(chained, 3);
-console.log('chained, 3 ==> ', chained);
-chained = push(chained, 2);
-console.log('chained, 2 ==> ', chained);
-chained = push(chained, 1);
-console.log('chained, 1 ==> ', chained);
-chained = push(chained, 8) // === 8 -> 1 -> 2 -> 3 -> null
-console.log('chained, 8 ==> ', JSON.stringify(chained, null, 2));
+// var chained = null
+// console.log('chained ==> ', chained);
+// chained = push(chained, 3);
+// console.log('chained, 3 ==> ', chained);
+// chained = push(chained, 2);
+// console.log('chained, 2 ==> ', chained);
+// chained = push(chained, 1);
+// console.log('chained, 1 ==> ', chained);
+// chained = push(chained, 8) // === 8 -> 1 -> 2 -> 3 -> null
+// console.log('chained, 8 ==> ', JSON.stringify(chained, null, 2));
+
+console.log(buildOneTwoThree());
