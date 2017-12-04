650f5920b7cb5f17fee3e3d6df1409e7a03ee998
Lisa Gee <zhusufeng@gmail.com>
2017-12-03 23:09:10 -0800

    Insert new exercise binarySearchNormal

---
+++ binarySearchNormal.js
@@ -0,0 +1,8 @@
+/* Binary Search Normal */
+/*
+  Given a sorted array and a number
+  Search for the index of the number
+  in the sorted array
+  If the number is not in the sorted
+  array, return -1
+*/
