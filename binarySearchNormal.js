0c0602f8adb809a1b2c3cde510afab1e57545459
Lisa Gee <zhusufeng@gmail.com>
2017-12-03 23:11:29 -0800

    Add OICE for binarySearchNormal

--- binarySearchNormal.js
+++ binarySearchNormal.js
@@ -5,4 +5,25 @@
   in the sorted array
   If the number is not in the sorted
   array, return -1
+
+  For this version, do not use recursion
+  Use a loop
+*/
+
+/*
+O: Number
+I: Sorted array, Number
+C: 
+  Time: Logarithmic
+  Space: Constant
+E:
+  [0, 3, 5, 7, 9], 7 => 3
+  [5, 10, 15, 20], 10 => 2
+  [0, 1, 2, 3, 4], 5 => -1
+  [0], 0 => 0
+  [] => -1
+  [0], 5 => -1
+
+General Plan
+
 */
