95826087adeff609d3cf6967d27cfcf28919a5c3
Lisa Gee <zhusufeng@gmail.com>
2017-12-10 22:19:19 -0800

    Create Sets from arrays & vars

--- sets/beginSet.js
+++ sets/beginSet.js
@@ -31,3 +31,17 @@ console.log(myFirstSet.size); // 3
 
 var mySecondSet = new Set([1, 2, 3]);
 console.log(mySecondSet);
+
+////////////////////////////////////
+
+var myThirdSet = new Set([1, 1, 1, 2, 3]);
+console.log('This is my third', myThirdSet);
+
+////////////////////////////////////
+
+var fourArray = [1, 1, 1, 2, 3, 4, 5]
+var myFourthSet = new Set(fourArray);
+console.log('This is my fourth', myFourthSet);
+
+console.log(myFourthSet.has(3)); // true
+console.log(myFourthSet.has(300)); // false
