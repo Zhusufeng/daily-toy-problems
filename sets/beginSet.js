446a69bacaa8544a7eeb7cd9086a2f5be7a292c8
Lisa Gee <zhusufeng@gmail.com>
2017-12-10 22:10:22 -0800

    Use the add, has, & size methods for Set

--- sets/beginSet.js
+++ sets/beginSet.js
@@ -1,10 +1,31 @@
 var myFirstSet = new Set();
 
-myFirstSet.add(1);
+myFirstSet.add(100);
 console.log(myFirstSet);
 
+myFirstSet.add(200);
+console.log(myFirstSet);
+
+myFirstSet.add('three hundred');
+console.log(myFirstSet);
+
+var four = { a: 400, b: 500}
+myFirstSet.add(four);
+console.log(myFirstSet);
+
+myFirstSet.add(four); // Does not add since already inside
+console.log(myFirstSet);
+
+console.log(myFirstSet.has(100)); // true
+console.log(myFirstSet.has(400)); // false
+console.log(myFirstSet.has(10 * 10)); // true
+console.log(myFirstSet.has('THREE HUNDRED'.toLowerCase())); // true
+console.log(myFirstSet.has(four)); // true
+
+console.log(myFirstSet.size); // four 
 
 
+////////////////////////////////////
 
 var mySecondSet = new Set([1, 2, 3]);
 console.log(mySecondSet);
