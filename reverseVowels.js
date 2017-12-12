f2f7e4338306cc8e6fde1afa4d83c4b34ad26e8e
Lisa Gee <zhusufeng@gmail.com>
2017-12-12 14:37:23 -0800

    Create general plan for reverseVowels

--- reverseVowels.js
+++ reverseVowels.js
@@ -23,6 +23,20 @@ E:
   "aeiou" => "uoiea"
   "laiu" => "luia"
   "luau" => "luau"
+
+General Plan
+  Split string into an array (so it is mutable)
+  Iterate through array
+    Collect vowels by saving them in an array 'vowels' with key of counter and values of char, index
+    Ie. vowels = [
+      {char: 'e', index: 1},
+      {char: 'o', index: 4},      
+    ]
+
+  While i is less than j
+    Do swaps outward to in updating the array
+    Start i at 0 and j as length - 1 of vowels
+    Increment i and decrement j
 */
 
 var reverseVowels = function(s) {
