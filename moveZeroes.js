80f0910f4bcb14a87efcf961243afe37692fc2cd
Lisa Gee <zhusufeng@gmail.com>
2017-12-11 17:42:39 -0800

    Add tests and idea #2 for moveZeroes

--- moveZeroes.js
+++ moveZeroes.js
@@ -9,7 +9,7 @@ Minimize the total number of operations.
 */
 
 /*
-O: Array of nums
+O: Nothing
 I: Array of nums
 C: 
   Time: Linear
@@ -43,4 +43,24 @@ Idea #1
 [1, 3, 12, 0, 0] before loop, i = 2
 [1, 3, 12, 0, 0] after loop, i = 2
 Algorithm should complete since it made it to endIndex
+
+Idea #2
+[0, 1, 0, 3, 12] => [1, 3, 12, 0, 0]
+
+Count zeroes as I go
+Iterate through the array til end, incrementing by 1 each loop
+  If array at current index is 0
+    Splice it out
+    Increment counter
+    i--
+    Update endIndex by -1
+Iterate til counter, incrementing by 1 each loop
+  Push 0s into array 
 */
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0]));
+console.log(moveZeroes([]));
+console.log(moveZeroes([1, 2]));
+console.log(moveZeroes([0, 1, 0, 2, 0, 3]));
+console.log(moveZeroes([1, 2, 3, 0, 0, 0]));
