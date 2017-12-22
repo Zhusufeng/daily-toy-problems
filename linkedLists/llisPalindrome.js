2b08ad5d9ee27ebaeacffb68fd337bc01c735e65
Lisa Gee <zhusufeng@gmail.com>
2017-12-22 08:55:23 -0800

    Add some ideas for general plan for llisPalindrome

--- linkedLists/llisPalindrome.js
+++ linkedLists/llisPalindrome.js
@@ -16,6 +16,20 @@ E:
   'o' -> 't' -> 'a' -> 't' -> 'r' -> null     => false
   'a' -> null                                 => ?
   'a' -> 'a' -> null                          => true
+
+General Plan
+Idea #1
+  Iterate through linked list pushing values into an array
+  Iterate through array checking if i (start from beginning) equals j (start from end)
+    If it doesn't equal return false
+  Return true
+
+Idea #2
+  Iterate through linked list 
+    Create new linked list where you addToHead
+  Iterate through linked list
+    Check if linked list node equals the second linked list's node
+      If not return false
 */
 
 function ListNode(val) {
