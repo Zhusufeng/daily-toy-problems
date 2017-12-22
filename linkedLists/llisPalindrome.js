bdeab8f1a11e44ceff23dc1fda3bdfd0a15b0046
Lisa Gee <zhusufeng@gmail.com>
2017-12-22 08:48:22 -0800

    Add OICE for ll isPalindrom

--- linkedLists/llisPalindrome.js
+++ linkedLists/llisPalindrome.js
@@ -2,6 +2,22 @@
 Given a singly linked list, determine if it is a palindrome. 
 Follow up: Could you do it in O(n) time and O(1) space?
 */
+
+/*
+O: Boolean
+I: Linked list
+C: 
+  Time:
+  Space:
+E:
+  'a' -> 'c' -> 'a' -> null                   => true
+  'o' -> 't' -> 'a' -> 't' -> 'o' -> null     => true
+  'a' -> 'c' -> 'e' -> null                   => false
+  'o' -> 't' -> 'a' -> 't' -> 'r' -> null     => false
+  'a' -> null                                 => ?
+  'a' -> 'a' -> null                          => true
+*/
+
 function ListNode(val) {
   this.val = val;
   this.next = null;
