0a051b2f3fd4167d10daaf78cd9a1ec822554e80
Lisa Gee <zhusufeng@gmail.com>
2017-12-23 23:42:47 -0800

    Add OICE, start general plan to findRobotPaths

--- findRobotPaths.js
+++ findRobotPaths.js
@@ -7,6 +7,43 @@
   to the bottom right corner.
  */
 
+ /*
+12.23.17
+O: Number
+I: Number (grid size)
+C: 
+  Space: Constant
+  Time: Linear
+E:
+ n = 1, 1
+ n = 2, 2
+ n = 3, 12
+
+ if n is 3
+ [
+  [f, f, f],
+  [f, f, f],
+  [f, f, f]
+ ]
+
+General Plan
+  Create the grid based off of n
+  Create counter which is 0
+  
+  Create subroutine to countPaths
+    If coordinates equal n - 1, n - 1, we made it to the bottom
+      Increment counter
+    If coordinates have been visited before
+      Return
+    Toggle coordinates as visited
+    
+
+
+  Call subroutine countPaths with i & j (which are locations in the matrix)
+  Return counter
+  
+*/
+
 var makeGrid = function(n) {
   var grid = [];
   for (var i = 0; i < n; i++) {
@@ -23,3 +60,4 @@ var makeGrid = function(n) {
   };
   return grid;
 };
+
