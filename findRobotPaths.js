93a3bf191342c270b98d0a4fcec8bca9546f9d0e
Lisa Gee <zhusufeng@gmail.com>
2017-12-23 23:49:16 -0800

    Add coordinate validity checks & move u, d, l, r

--- findRobotPaths.js
+++ findRobotPaths.js
@@ -31,13 +31,20 @@ General Plan
   Create counter which is 0
   
   Create subroutine to countPaths
+    Check if coordinates are valid - If i or j is less than 0 or if i or j is greater than or equal to n
+      Return
     If coordinates equal n - 1, n - 1, we made it to the bottom
       Increment counter
+      Return
     If coordinates have been visited before
       Return
     Toggle coordinates as visited
-    
-
+    Start moving across the board
+    Go right, countPaths i, j + 1
+    Go down, countPaths i + 1, j
+    Go left, countPaths i, j - 1
+    Go up, countPaths i - 1, j
+    Toggle coordinates as visited
 
   Call subroutine countPaths with i & j (which are locations in the matrix)
   Return counter
