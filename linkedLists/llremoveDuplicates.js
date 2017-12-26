ddacd00402b13fcdc3fb302daf9d1c0c8a898a00
Lisa Gee <zhusufeng@gmail.com>
2017-12-26 12:50:35 -0800

    Add OICE to removeDuplicates

--- linkedLists/llremoveDuplicates.js
+++ linkedLists/llremoveDuplicates.js
@@ -10,6 +10,24 @@ If the passed in list is null/None/nil, simply return null.
 Note: Your solution is expected to work on long lists. Recursive solutions may fail due to stack size limitations.
 */
 
+/*
+O: Linked list
+I: Linked list
+   assumption: sorted in increasing order
+C:
+  Time: Linear
+  Space: Constant
+E:
+  null                                    => null
+  3 -> 3 -> 3 -> null                     => 3 -> null
+  0 -> 1 -> 2 -> 3 -> 4 -> 4 -> 4 -> null => 0 -> 1 -> 2 -> 3 -> 4 -> null
+
+General Plan
+  If head is null, return null
+  Create prevNode
+  
+*/
+
 function Node(data) {
   this.data = data;
   this.next = null;
@@ -19,3 +37,41 @@ function removeDuplicates(head) {
   // Your code goes here.
   // Remember to return the head of the list.
 }
+
+let sample1 = {
+  data: 3,
+  next: {
+    data: 3,
+    next: {
+      data: 3,
+      next: null
+      }
+    }
+  }
+};
+
+let sample2 = {
+  data: 0,
+  next: {
+    data: 1,
+    next: {
+      data: 2,
+      next: {
+        data: 3,
+        next: {
+          data: 4,
+          next: {
+            data: 4,
+            next: {
+              data: 4,
+              next: null
+            }
+          }
+        }
+      }
+    }
+  }
+};
+console.log(removeDuplicates(null)); // null
+console.log(removeDuplicates(sample1)); // 3 -> null
+// console.log(removeDuplicates(sample2)); // 0 -> 1 -> 2 -> 3 -> 4 -> null
