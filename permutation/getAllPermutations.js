7c7edc13bf4c89fd88c68788cb87caa7ff5ca710
Lisa Gee <zhusufeng@gmail.com>
2018-01-04 19:04:23 -0800

    Add OICE to getAllPermutations

--- permutation/getAllPermutations.js
+++ permutation/getAllPermutations.js
@@ -12,3 +12,40 @@ For example,
   [3,2,1]
 ]
 */
+
+/*
+O: Array of arrays
+I: Array
+C:
+  Time: Quadratic
+  Space: 
+E:
+
+Input: [1,2,3]
+Output: [
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+Input: [0]
+Output: [ 
+  [0] 
+]
+
+Input: [0,1]
+Output: [
+  [0,1],
+  [1,0]
+]
+
+General Plan:
+
+*/
+
+var permute = function(nums) {
+    
+};
