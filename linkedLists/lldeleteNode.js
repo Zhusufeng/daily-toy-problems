8f0d5ca1aa2fc9d8349c37a31724a157f95c4b29
Lisa Gee <zhusufeng@gmail.com>
2018-01-04 20:32:25 -0800

    Add new linked list deleteNode prompt

---
+++ linkedLists/lldeleteNode.js
@@ -0,0 +1,9 @@
+/*
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Ie.
+1 -> 2 -> 3 -> 4
+You are given the third node with the value 3
+The linked list should now look like:
+1 -> 2 -> 4
+*/
