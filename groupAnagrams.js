e1ee05674d891374e62dea00bface058373a1994
Lisa Gee <zhusufeng@gmail.com>
2018-01-07 20:02:28 -0800

    Add 1st idea for groupAnagrams

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -44,4 +44,28 @@ E:
 
 General Plan
 
+Idea #1
+  Create a permutation obj
+  Create a holder obj (that will have arrays as values)
+  Create result as an empty arr
+  Sort the array
+  Go through given array
+    If its the 1st element
+      Make a set of permutations bl -> bl, lb
+      Save this set to the permutation obj using the string as the key and the set as the val
+      Save it to the holder obj using the string as the key and value as string in an array
+    Else
+      Iterate through permutation obj and see if string is inside any of the sets
+        If it is
+          Get the key of the permutation obj
+          Save it to the holder obj using the key from permutation obj and push it to the array
+        Else
+          Make a set of permutations bl -> bl, lb
+          Save this set to the permutation obj using the string as the key and the set as the val
+          Save it to the holder obj using the string as the key and value as string in an array
+    Iterate through the holder obj
+      Push each value to result array
+    Return result
+
+Idea #2
 */
