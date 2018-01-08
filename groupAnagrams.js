b4c64259d36d0208b34a94140df42809949ca99d
Lisa Gee <zhusufeng@gmail.com>
2018-01-07 19:50:06 -0800

    Add general plan for groupAnagrams

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -11,3 +11,37 @@ Return:
 ]
 Note: All inputs will be in lower-case.
 */
+/*
+O: Array of arrays which contain strings
+I: Array of strings
+C:
+  Time: 
+  Space: 
+E:
+  Input:
+  ["eat", "tea", "tan", "ate", "nat", "bat"]
+  Output:
+  [
+    ["ate", "eat","tea"],
+    ["nat","tan"],
+    ["bat"]
+  ]
+
+  Input: 
+  ["at", "ta"]
+  Output:
+  [
+    ["at", "ta"]
+  ]
+
+  Input: 
+  ["bl", at", "ta", "lb']
+  Output:
+  [
+    ["at", "ta"],
+    ["bl", "lb"]
+  ]
+
+General Plan
+
+*/
