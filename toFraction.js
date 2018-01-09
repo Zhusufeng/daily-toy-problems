3db790e3780f7a74434f5b1c206e8b5f3bcc8e6b
Lisa Gee <zhusufeng@gmail.com>
2018-01-09 09:22:22 -0800

    Add new prompt toFraction

---
+++ toFraction.js
@@ -0,0 +1,17 @@
+/**
+ * Write a function that takes a number as its argument and
+ * returns a string that represents that number's simplified fraction.
+ *
+ * Example: toFraction(0.5) === '1/2'
+ *
+ * Whole numbers and mixed fractions should be returned as irregular fractions
+ *
+ * Example: toFraction(4.0) === '4/1'
+ *
+ * Example: toFraction(3.5) === '7/2'
+ *
+ */
+
+var toFraction = function(num) {
+
+}
