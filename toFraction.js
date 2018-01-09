016c72d8f2fa67327c329b0672cca2a88717db07
Lisa Gee <zhusufeng@gmail.com>
2018-01-09 09:43:27 -0800

    Make plan for converting hundredths in toFraction

--- toFraction.js
+++ toFraction.js
@@ -26,6 +26,40 @@
     0.33 => '1/3'
     0.875 => '7/8'
 
+  General Plan
+
+  Focus on tenths first
+  0.1 => 1/10
+  0.2 => 2/10 -> 1/5
+  0.3 => 3/10
+  0.4 => 4/10 -> 2/5
+  0.5 => 5/10 -> 1/2
+
+  How do you do it in math?
+    0.5 => '1/2'
+    5 is numerator
+    10 is denominator
+
+  Next hundredths...
+  Example:
+    0.75
+    75 is numerator
+    100 is denominator
+    Check if numerator & denominator are divisible by same num - up to denom (100)
+    1, 2, 3, 4, 5 (both divisible)
+    Divide both by 5
+    15 is numerator
+    20 is denominator
+    Check if numerator & denominator are divisible by same num - up to denom (20)
+    1, 2, 3, 4, 5 (both divisible)
+    Divide both by 5
+    3 is numerator
+    4 is denominator
+    Check if numerator & denominator are divisible by same num - up to denom (4)
+    No, so return
+    numerator + / + denominator
+    3 + / + 4
+
 */
 var toFraction = function(num) {
 
