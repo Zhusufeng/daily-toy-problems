a7e734fcbb290690ff4762130c203783a9e4e134
Lisa Gee <zhusufeng@gmail.com>
2018-01-08 20:25:22 -0800

    Add new prompt, commonChar

---
+++ commonChars.js
@@ -0,0 +1,13 @@
+/*
+
+ Given 2 strings, return a string of the common characters.
+ The order of the returned string should be based off of the 1st argument.
+ The returned string should have no duplicate characters.
+ Do not count spaces as a character.
+
+ */
+
+
+var commonChars = function(str1, str2) {
+  // TODO: Your code here!
+};
