29317e13d0bdb90bfc4eaeaa97bcb0f81daa2196
Lisa Gee <zhusufeng@gmail.com>
2018-01-10 09:39:20 -0800

    Add some analysis of movement in rotateMatrix

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -2,6 +2,36 @@
   Rotate a matrix 90 degrees
 */
 
+/*
+  O: Matrix
+  I: Matrix
+  C: 
+    Time:
+    Space:
+  E:
+
+  var matrix = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9, 10,11,12],
+    [13,14,15,16]
+  ];
+
+  var matrix = [ 
+   [13,9,5,1],
+   [14,10,6,2],
+   [15,11,7,3],
+   [16,12,8,4]
+  ]
+
+  Look at first row -> rotated first row
+  0,0 -> 0,3   i + 0, i + 3
+  0,1 -> 1,3   i + 1, i + 2
+  0,2 -> 2,3   i + 2, i + 1
+  0,3 -> 3,3   i + 3, i + 0
+
+*/
+
 var rotateMatrix = function(matrix) {
   
 };
