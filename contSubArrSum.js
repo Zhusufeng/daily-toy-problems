587a7b26b27e843444b58f1ffcd3c83b12507d2a
Lisa Gee <zhusufeng@gmail.com>
2018-01-15 22:34:21 -0800

    Make a strategy for example 2 for contSubArrSum

--- contSubArrSum.js
+++ contSubArrSum.js
@@ -14,8 +14,14 @@
 */
 
 var checkSubarraySum = function(nums, k) {
-  for (let i = 0; i < nums.length - 1; i++) {
-    if ((nums[i] + nums[i + 1]) % k === 0) {
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && (nums[i] + nums[i - 1]) % k === 0) {
+      return true;
+    }
+    sum += nums[i];
+    if (sum % k === 0) {
       return true;
     }
   }
@@ -27,6 +33,9 @@ var checkSubarraySum = function(nums, k) {
   Strategy for Example 1
     Iterate through argument 1 array. Sum current item and next item. If sum mod k is 0, return true
 
+  Strategy for Example 2
+    Iterate through argument 1 array. Sum up all items... If sum mod k is 0, return true
+
   Time Complexity
     Linear
 */
@@ -36,3 +45,4 @@ const example1 = [23, 2, 6, 4, 7];
 const k1 = 6;
 
 console.log(checkSubarraySum(example1, 8));
+console.log(checkSubarraySum(example1, 42));
