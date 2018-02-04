8fd21781c0d3bac17656071ef561f6ea6bfc8570
Lisa Gee <zhusufeng@gmail.com>
2018-02-03 23:36:21 -0800

    Add steps to countWaterBlocks

--- countWaterBlocks.js
+++ countWaterBlocks.js
@@ -35,10 +35,45 @@
       While the two pointers are not equal to each other, check if the left wall is higher than the right. If it is, add high left wall - current value at left pointer to counter. Otherwise add high right wall - current value at right pointer to counter.
 
     Big-O
+      Time: O(n)- traverse array of walls once
+      Space: O(1)      
 
     Steps
+      Given [4, 1, 1, 5, 2, 3]
+      i = 0, j = 5, counter = 0
+      leftWall = 4, rightWall = 3
+      rightWall is less than leftWall
+        counter += rightWall - arr[5] aka 3 - 3 aka 0
+        Decrement j
+      i = 0, j = 4, counter = 0
+      leftWall = 4, rightWall = 3 or 2
+      rightWall is less than leftWall
+        counter += rightWall - arr[4] aka 3 - 2 aka 1
+        Decrement j
+      i = 0, j = 3, counter = 1
+      leftWall = 4, rightWall = 3 or 5
+      leftWall is less than rightWall
+        counter += leftWall - arr[0] aka 4 - 4 aka 0
+        Increment i
+      i = 1, j = 3, counter = 1
+      leftWall = 4 or 1, rightWall = 5 or 5
+      leftWall is less than rightWall
+        counter += leftWall - arr[1] aka 4 - 1 aka 3
+        Increment i
+      i = 2, j = 3, counter = 4
+      leftWall = 4 or 1, rightWall = 5 or 5
+      leftWall is less than rightWall
+        counter += leftWall - arr[2] aka 4 - 1 aka 3
+        Increment i
+      i = 3, j = 3, counter = 7
+      leftWall = 4 or 5, rightWall = 5 or 5
+        counter += rightWall - arr[3] aka 5 - 5 aka 0
+        Increment i
+      i is now greater than j, exit loop
+      return counter, which is 7
 
     Skeleton
+
 */
 
 var waterBlocks = function (blocks) {
