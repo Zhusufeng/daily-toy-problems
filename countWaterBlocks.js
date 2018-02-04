c7fa1dc10699ddd94cda19c77fc07fbaa536b4f4
Lisa Gee <zhusufeng@gmail.com>
2018-02-03 22:55:28 -0800

    Add more input & output examples in countWaterBlocks

--- countWaterBlocks.js
+++ countWaterBlocks.js
@@ -12,7 +12,7 @@
     # # # # - #
     # # # # # #
     -----------------------------------------
-    
+    Inputs & Outputs
     [4, 4, 4, 4] => 0
     [4, 4, 4, 3] => 0
     [4, 3, 2, 1] => 0
@@ -20,62 +20,11 @@
     [3, 4, 4, 3] => 0
     [1, 4, 4, 1] => 0
     
-    1stHighPt
-    2ndHighPt
-    If there is no 2ndHighPt
-      There is 0 water (it all flows down)
-    -----------------------------------------
-    
-    [4, 4, 3, 4] => 1 
-      1stHighPt @ 1 = 4
-      2ndHighPt @ 3 = 4
-      
-      
-      
-    [4, 3, 3, 4] => 2
-      1stHighPt @ 0 = 4
-      2ndHighPt @ 3 = 4
-      
-      Index 0: 0 water
-      Index 1: 1 water  
-      Index 2: 1 water
-      Index 3: 0 water
-      
-      Find the lower of the 2 high points
-      Iterate through the array that were inbetween the high points
-        Subtract arr[lower] - arr[i]
-        Add the difference to waterCount
-      
-    [4, 3, 2, 4] => 3
-      1stHighPt @ 0 = 4
-      2ndHighPt @ 3 = 4
-      
-    [4, 3, 1, 4] => 4
-      1stHighPt @ 0 = 4
-      2ndHighPt @ 3 = 4
-      
-    [4, 1, 1, 4] => 6
-      1stHighPt @ 0 = 4
-      2ndHighPt @ 3 = 4    
-    
-    -----------------------------------------
-  
-  General Plan
-    Create waterCount, set to 0
-    
-    Loop through array
-      Find 1stHighPt
-      Find 2ndHighPt
-        If there is no 2ndHighPt
-          There is 0 water (it all flows down)
-        Find the lower of the 2 high points
-        Iterate through the array that were inbetween the high points
-          Subtract arr[lower] - arr[i]
-          Add the difference to waterCount
-      Set 1stHighPt equal to 2ndHighPt
-      Set 2ndHighPt to null
-      Repeat
-
+    [5, 1, 1, 5] => 8
+    [4, 1, 1, 5, 2, 3] => 7
+    [5, 4, 1, 1, 4, 5] => 10
+    [4, 1, 5, 2, 4] => 5
+    [5, 1, 1, 4, 1, 5] => 13
 */
 
 var waterBlocks = function (blocks) {
