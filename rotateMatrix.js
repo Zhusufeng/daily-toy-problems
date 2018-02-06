f969e0e99f165db8b7a6b96a0aa3a9eee6fb1e68
Lisa Gee <zhusufeng@gmail.com>
2018-02-05 22:34:08 -0800

    Find a pattern in rotateMatrix

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -30,7 +30,7 @@
   Look at first row -> rotated first row
   before -> rotated
   [m][n] 
-  [0][0] -> [0][3]   m + 0, n + 3
+  [0][0] -> [0][3]   m + 0, n + 3 
   [0][1] -> [1][3]   m + 1, n + 2
   [0][2] -> [2][3]   m + 2, n + 1
   [0][3] -> [3][3]   m + 3, n + 0
@@ -40,9 +40,20 @@
   [1][2] -> [2][2]   m + 1, n + 0
   [1][3] -> [3][2]   m + 2, n - 1
 
-  Hypothesis: So per row, 
+  [2][0] -> [0][1]   m - 2, n + 1
+  [2][1] -> [1][1]   m - 1, n + 0
+  [2][2] -> [2][1]   m + 0, n - 1
+  [2][3] -> [3][1]   m - 1, n - 2
 
-  
+  [3][0] -> [0][0]   m - 3, n + 0
+  [3][1] -> [1][0]   m - 2, n - 1
+  [3][2] -> [2][0]   m - 1, n - 2
+  [3][3] -> [3][0]   m + 0, n - 3
+
+  pattern:
+    n becomes m when rotated
+    if m was 0, n will be matrix.length - 1 and decrement
+    
 
 
 
