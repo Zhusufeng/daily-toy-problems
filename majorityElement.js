b03baba7a0a56da30d7162767307c4d0e2aed652
Lisa Gee <zhusufeng@gmail.com>
2018-02-10 00:01:13 -0800

    Create inputs & outputs at majorityElement

--- majorityElement.js
+++ majorityElement.js
@@ -1,5 +1,28 @@
 /*
-Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+  Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
-You may assume that the array is non-empty and the majority element always exist in the array.
+  You may assume that the array is non-empty and the majority element always exist in the array.
+*/
+
+/*
+  Inputs and Outputs
+    const input1 = [6, 5, 5, 5, 5, 5, 5, 5, 6]
+    n = 9
+    obj = {
+      6: 2
+      5: 7 
+    }
+    9/2 = 4+ 
+    4+ < 7
+    const majority1 = 5
+
+  const input2 = [6, 5, 5, 5, 5, 6]
+  n = 6
+  obj = {
+    6: 2,
+    5: 4
+  }
+  6/2 = 3
+  3 < 4
+  const majority2 = 5
 */
