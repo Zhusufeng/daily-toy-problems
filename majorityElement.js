7b85b992644027e07a62253333d89127aacb513e
Lisa Gee <zhusufeng@gmail.com>
2018-02-10 00:07:30 -0800

    Add another input, strategy, constraints, skeleton in majorityElement

--- majorityElement.js
+++ majorityElement.js
@@ -25,4 +25,33 @@
   6/2 = 3
   3 < 4
   const majority2 = 5
+
+  const input3 = [5, 5, 5, 5, 5, 6, 6, 6, 6]
+  n = 9
+  obj = {
+    5: 5
+    6: 4
+  }
+  9/2 = 4+
+  4+ < 5
+  const majority3 = 5
+
+  Strategy
+    Iterate through the array and create a hash & count their instances. Find n/2. Iterate over hash to find majority (which is n/2 > majority)
+
+  Big-O
+
+
+
+  Constraints
+    The array is non-empty
+    The majority element always exist in the array
+
+  Steps
+
+  Skeleton
+    const majorityElement = (nums) => {
+      // The array is non-empty
+      // The majority element always exist in the array
+    };
 */
