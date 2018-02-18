db11b104b51d1fa63911d5b21cd386de012bc427
Lisa Gee <zhusufeng@gmail.com>
2018-02-17 23:51:37 -0800

    Add inputs and puts to majorityEl2

---
+++ majorityEl2.js
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/majority-element-ii/description/
+
+/*
+  Inputs & Outputs, Steps
+    const input1 = [1, 3, 3, 3, 3, 3]
+      hash = {1: 1, 3: 5}
+      n = 6
+      max = n/3 = 6/3 = 2
+    const output1 = 3
+
+    const input2 = [1, 1, 1, 3, 3, 3, 3, 3]
+      hash = {1: 3, 3: 5}
+      n = 8
+      max = n/3 = 8/3 = 2.67
+    const output2 = 1, 3
+
+  Strategy
+
+  Big-O
+
+  Constraints
+
+  Pattern
+*/
