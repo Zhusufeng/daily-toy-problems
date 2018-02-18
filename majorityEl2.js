de911d95e2ab7e229f2473e9e83b141c21534f46
Lisa Gee <zhusufeng@gmail.com>
2018-02-17 23:55:16 -0800

    Add another input & output to majorityEl2

--- majorityEl2.js
+++ majorityEl2.js
@@ -6,15 +6,22 @@
       hash = {1: 1, 3: 5}
       n = 6
       max = n/3 = 6/3 = 2
-    const output1 = 3
+    const output1 = [3]
 
     const input2 = [1, 1, 1, 3, 3, 3, 3, 3]
       hash = {1: 3, 3: 5}
       n = 8
       max = n/3 = 8/3 = 2.67
-    const output2 = 1, 3
+    const output2 = [1, 3]
+
+    const input3 = [1, 1, 1, 3, 3, 3, 3, 3, 7]
+      hash = {1: 3, 3: 5, 7: 1}
+      n = 9
+      max = n/3 = 9/3 = 3
+    const output3 = [3]
 
   Strategy
+    
 
   Big-O
 
