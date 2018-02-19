715a7b0bf94f1924c0fe23cc366677963f58d091
Lisa Gee <zhusufeng@gmail.com>
2018-02-18 23:26:46 -0800

    Remove text, link to prompt

--- linkedLists/llmoveNode.js
+++ linkedLists/llmoveNode.js
@@ -1,14 +1,5 @@
 /*
-Write a MoveNode() function which takes the node from the front of the source list and moves it to the front of the destintation list. You should throw an error when the source list is empty. For simplicity, we use a Context object to store and return the state of the two linked lists. A Context object containing the two mutated lists should be returned by moveNode.
-
-MoveNode() is a handy utility function to have for later problems.
-
-JavaScript
-
-var source = 1 -> 2 -> 3 -> null
-var dest = 4 -> 5 -> 6 -> null
-moveNode(source, dest).source === 2 -> 3 -> null
-moveNode(source, dest).dest === 1 -> 4 -> 5 -> 6 -> null
+  Prompt: http://www.codewars.com/kata/linked-lists-move-node
 */
 
 /*
