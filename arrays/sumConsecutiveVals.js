f533d90ecb9633a5cd418d914699c4ae8779edd3
Lisa Gee <zhusufeng@gmail.com>
2018-02-21 17:25:48 -0800

    Add example inputs/outputs, a strategy, its big-O & constraints to sumConsecVal

--- arrays/sumConsecutiveVals.js
+++ arrays/sumConsecutiveVals.js
@@ -1,15 +1,32 @@
 /*
-  Prompt: Verbal
+  Prompt: Verbal - John's Challenge 1
 */
 
 /*
   Input & Output
+    const input1 = [1, 2, 2, 2, 5, 8];
+    const output1 = [1, 6, 5, 8];
+
+    const input2 = [1, 2, 2, 2, 5, 2, 8];
+    const output2 = [1, 6, 5, 2, 8];
+
+    const input3 = [1, 1, 1, 2, 2, 2, 3, 4]
+    const output3 = [3, 6, 3, 4]
 
   Strategy
+    Iterate thru input, if el does not eql lastVal, push el to results. Otherwise, pop off results and push it plus the el into results
 
   Big-O
+    Time: Linear - O(n)
+    Space: Constant - O(1) additional
 
   Constraints
+    Input will never be empty
+    Input will contain integers
+
+  Steps
+    Given [1, 2, 2, 2, 5, 2, 8]
+    
 
   Skeleton
 */
