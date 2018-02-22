14f345b9babccd20312fddaef52813de943a629e
Lisa Gee <zhusufeng@gmail.com>
2018-02-21 17:30:30 -0800

    Add illustrative steps for strategy on sumConsecutiveVals

--- arrays/sumConsecutiveVals.js
+++ arrays/sumConsecutiveVals.js
@@ -26,7 +26,38 @@
 
   Steps
     Given [1, 2, 2, 2, 5, 2, 8]
-    
+    lastVal = null
+
+    i = 0, input[0] = 1, 1 !== null
+    lastVal = 1
+    results = [1]
+
+    i = 1, input[1] = 2, 2 !== 1
+    lastVal = 2
+    results = [1, 2]
+
+    i = 2, input[2] = 2, 2 === 2
+    updatedVal = results.pop() = 2
+    results.push(updatedVal + input[i])
+    results = [1, 4]
+
+    i = 3, input[3] = 2, 2 === 2
+    updatedVal = results.pop() = 4
+    results.push(updatedVal + input[i])
+    results = [1, 6]
+
+    i = 4, input[4] = 5, 5 !== 2
+    lastVal = 5
+    results = [1, 6, 5]
+
+    i = 5, input[5] = 2, 2 !== 5
+    lastVal = 2
+    results = [1, 6, 5, 2]
+
+    i = 6, input[6] = 8, 8 !== 2
+    lastVal = 8
+    results = [1, 6, 5, 2, 8]
+
 
   Skeleton
 */
