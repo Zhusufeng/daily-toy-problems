31341567eae12ada4f993e737f0f3f6c115d95a5
Lisa Gee <zhusufeng@gmail.com>
2018-02-22 18:50:32 -0800

    Add new prompt to do from John

---
+++ toDo/sumConsecutiveVals2.js
@@ -0,0 +1,11 @@
+##### Challenge 2
+
+Given an array and a starting edge and an ending edge sum consecutive like numbers, if there are numbers that are also like prior to the starting edge go back and add those in as well (given [1,1,1,2,2,2] and a starting edge with index 1 and an ending edge with index 4 return [3,6]
+
+
+       S=2         E=6
+       |           |
+[2, 1, 1, 1, 4, 3, 6, 6, 1] => [2, 3, 4, 3, 12, 1]
+
+
+Start / End = Edge
