a559b0c6265861dd2d2a21b9ea6b15d5723f3ade
Lisa Gee <zhusufeng@gmail.com>
2018-02-22 23:43:57 -0800

    Add input/output example for isLanguageDivese

--- toDo/isLanguageDiverse.js
+++ toDo/isLanguageDiverse.js
@@ -2,7 +2,7 @@
   Prompt: https://www.codewars.com/kata/coding-meetup-number-13-higher-order-functions-series-is-the-meetup-language-diverse
 */
 
-var input1 = [
+const input1 = [
   { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'Python' },
   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'Ruby' },
   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 43, language: 'Ruby' },
@@ -19,4 +19,14 @@ function isLanguageDiverse(list) {
 /*
   2.22.18
   Inputs & Outputs
+    const input1 = [
+      { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'Python' },
+      { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'Ruby' },
+      { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 43, language: 'Ruby' },
+      { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 95, language: 'JavaScript' },
+      { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 18, language: 'JavaScript' },
+      { firstName: 'Joao', lastName: 'D.', country: 'Portugal', continent: 'Europe', age: 25, language: 'JavaScript' }
+    ];
+
+    const output1 = true;
 */
