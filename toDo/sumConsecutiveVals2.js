b312761a72d5a1823dbcf9e4558697fbb39cdd38
Lisa Gee <zhusufeng@gmail.com>
2018-02-25 23:45:32 -0800

    Add input & output for sumConsecVals2

--- toDo/sumConsecutiveVals2.js
+++ toDo/sumConsecutiveVals2.js
@@ -1,3 +1,4 @@
+/*
 ##### Challenge 2
 
 Given an array and a starting edge and an ending edge sum consecutive like numbers, if there are numbers that are also like prior to the starting edge go back and add those in as well (given [1,1,1,2,2,2] and a starting edge with index 1 and an ending edge with index 4 return [3,6]
@@ -9,3 +10,13 @@ Given an array and a starting edge and an ending edge sum consecutive like numbe
 
 
 Start / End = Edge
+*/
+
+/*
+  Input & Output
+  const arr1 = [1,1,1,2,2,2];
+  const start = 1;
+  const end = 4;
+  const output1 = [3, 6];
+
+*/
