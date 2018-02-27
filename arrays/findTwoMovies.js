79f1d76368f95a86eed36ef1f739a0ad628ff5ef
Lisa Gee <zhusufeng@gmail.com>
2018-02-27 15:30:33 -0800

    Add strategy ideas, big-o & constraints for findTwoMovies

--- arrays/findTwoMovies.js
+++ arrays/findTwoMovies.js
@@ -12,11 +12,26 @@
     const movies2 = [100, 130, 120, 180];
     const output2 = false;
 
+    const time2 = 200;
+    const movies2 = [100, 130, 120, 180];
+    const output2 = false;
+
   Strategy
+    Idea #1
+      Iterate thru movies and then iterate again thru movies to see if you found the complement
+
+    Idea #2
+      Sort the array. Iterate thru movies and then binary search for complement
+
+    Idea #3
+      Create set from movies. Iterate thru set and use has to seek complement
 
   Big-O
+    Time: O(2n) - Linear
+    Space: O(1) additional
 
   Constraints
+    Should not use same movie twice!
 
   Steps
 
