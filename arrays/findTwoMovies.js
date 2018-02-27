76ec7a701530c2cd3a4896b3c3511dfdabc07177
Lisa Gee <zhusufeng@gmail.com>
2018-02-27 15:23:32 -0800

    Add input & output examples for findTwoMovies

--- arrays/findTwoMovies.js
+++ arrays/findTwoMovies.js
@@ -2,3 +2,23 @@
   Prompt: https://www.interviewcake.com/question/javascript/inflight-entertainment
 */
 
+/*
+  Inputs & Outputs
+    const time1 = 240;
+    const movies1 = [100, 140, 120, 180];
+    const output1 = true;
+
+    const time2 = 240;
+    const movies2 = [100, 130, 120, 180];
+    const output2 = false;
+
+  Strategy
+
+  Big-O
+
+  Constraints
+
+  Steps
+
+  Skeleton
+*/
