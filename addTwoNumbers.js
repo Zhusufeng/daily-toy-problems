2526c031127e81b93593b03979eee8321b706fab
Lisa Gee <zhusufeng@gmail.com>
2018-03-01 23:32:24 -0800

    Create input & output example for addTwoNumbers

---
+++ addTwoNumbers.js
@@ -0,0 +1,37 @@
+/*
+  Prompt: https://leetcode.com/problems/add-two-numbers/description/
+*/
+
+/*
+  Input & Output
+    const input1a = {
+      val: 1,
+      next: {
+        val: 2,
+        next: {
+          val: 3,
+          next: null
+        }
+      }
+    };
+    const input1b = {
+      val: 3,
+      next: {
+        val: 4,
+        next: {
+          val: 5,
+          next: null
+        }
+      }
+    };
+    const output1 = {
+      val: 4,
+      next: {
+        val: 6,
+        next: {
+          val: 8,
+          next: null
+        }
+      }
+    };
+*/
