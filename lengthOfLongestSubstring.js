da78ca9f7e07b071a173468ff08543bb90130787
Lisa Gee <zhusufeng@gmail.com>
2018-03-12 23:23:23 -0700

    Move firstNotRepeatChar, add new prompt wth inputs & outputs lengthOfLongest

---
+++ lengthOfLongestSubstring.js
@@ -0,0 +1,16 @@
+/*
+  Prompt: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+*/
+
+/*
+  Inputs & Outputs
+    const input1 = 'abcabab'
+    const output1 = 'abc'
+
+    const input2 = 'ooooo'
+    const output2 = 'ooooo'
+
+    const input3 = 'pwwkew'
+    const output3 = 'wke'
+    
+*/
