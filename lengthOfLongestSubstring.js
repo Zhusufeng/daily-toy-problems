4aa401c55f4115749eff0a4b2c9027c47591b27d
Lisa Gee <zhusufeng@gmail.com>
2018-03-12 23:31:02 -0700

    Create 1st strategy for lengthOfLongestSubstring

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -8,9 +8,28 @@
     const output1 = 'abc'
 
     const input2 = 'ooooo'
-    const output2 = 'ooooo'
+    const output2 = 'o'
 
     const input3 = 'pwwkew'
     const output3 = 'wke'
+
+  Strategy
+
+    Split string input into an array
+    Keep track of current substring in an array
+    Keep track of all unique substrings in substrings array
     
+    Idea #1
+      Iterate thru input array 
+        If current char is already in substring (indexOf)
+          Push current substring to substrings
+          Restart substring with current char as 1st element
+        Else 
+          Push current char to current substring
+      Iterate over substrings
+        Keep track of substring with longest length (Math.max)
+      Return longest substring
+
+
+  Big-O  
 */
