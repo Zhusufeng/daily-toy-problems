099c46f58e860a8bd6e52e75887837aad77f4278
Lisa Gee <zhusufeng@gmail.com>
2018-03-15 23:55:54 -0700

    Add new prompt with ex input & output

---
+++ trees/sumOfLeftLeaves.js
@@ -0,0 +1,35 @@
+/*
+  Prompt: https://leetcode.com/problems/sum-of-left-leaves/description/
+*/
+
+/*
+  Inputs & Outputs
+*/
+const input1 = {
+  val: 3,
+  left: {
+    val: 9,
+    left: null,
+    right: null
+  },
+  right: {
+    val: 20,
+    left: {
+      val: 15,
+      left: null,
+      right: null
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null
+    }
+  }
+};
+
+const output1 = 24;
+
+const input2 = {
+
+};
+
