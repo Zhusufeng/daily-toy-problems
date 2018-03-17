3b16f3f920ffe46a04eab55a8a9240226cc132d8
Lisa Gee <zhusufeng@gmail.com>
2018-03-16 23:50:52 -0700

    Add more inputs/outputs, strategy & big-o for maxProfit

--- arrays/maxProfit.js
+++ arrays/maxProfit.js
@@ -4,6 +4,26 @@
 
 /*
   Input & Output
+*/
     const input1 = [7, 1, 5, 3, 6, 4];
-    const output1 = 5
+    const output1 = 5;
+
+    const input2 = [8, 4, 7];
+    const output2 = 3;
+
+    const input3 = [5, 5, 5];
+    const output3 = 0;
+
+/*
+  Strategy
+    Keep track of the smallest number & iterate thru to find max
+
+  Big-O
+    Time: Linear
+    Space: Constant
+
+  Steps
+    Given [8, 4, 7] 
+
+  Pseudo
 */
