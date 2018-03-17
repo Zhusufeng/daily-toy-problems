d3ec670eadcff566d90faa7ffeede6ad1c37489b
Lisa Gee <zhusufeng@gmail.com>
2018-03-16 23:44:03 -0700

    Add new prompt maxProfit

---
+++ arrays/maxProfit.js
@@ -0,0 +1,9 @@
+/*
+  Prompt: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+*/
+
+/*
+  Input & Output
+    const input1 = [7, 1, 5, 3, 6, 4];
+    const output1 = 5
+*/
