5e5f16c9b734d0325bbe282cb2a3ddd8bc23fc6e
Lisa Gee <zhusufeng@gmail.com>
2018-03-19 15:29:18 -0700

    Add idea #1 under strategy in longestPalindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -14,8 +14,20 @@
     const input3 = 'abatacocat';
     const output3 = 'tacocat';
 
+    const input4 = 'tacocatacocat';
+    const output4 = 'tacocatacocat';
+
 /*
   Strategy
+    Idea #1 
+      Iterate thru string
+        Iterate thru remaining string
+          Add substring to an array
+      Iterate thru array
+        Use a helper func isPalindrome to check if string inside is palindrome
+        Add palindromes to an array
+      Iterate thru array
+        Update longestPalindrome to longestPalindrome found
 
   Big-O
 
