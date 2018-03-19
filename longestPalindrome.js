40292ad2de4ce067769575bd1836ff3654765203
Lisa Gee <zhusufeng@gmail.com>
2018-03-19 15:18:42 -0700

    Add new prompt with example input & output, longestPalindrome

---
+++ longestPalindrome.js
@@ -0,0 +1,30 @@
+/*
+  Prompt: https://leetcode.com/problems/longest-palindromic-substring/description/
+*/
+
+/*
+  Input & Output
+*/
+    const input1 = 'ababad';
+    const output1 = 'bab'; // or 'aba'
+
+    const input2 = 'cbbd';
+    const output2 = 'bb';
+
+    const input3 = 'abatacocat';
+    const output3 = 'tacocat';
+
+/*
+  Strategy
+
+  Big-O
+
+  Constraints
+
+  Steps
+
+  Skeleton  
+    const longestPalindrome = s => {
+  
+    };
+*/
