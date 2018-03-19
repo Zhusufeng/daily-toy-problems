f4e984398283026283451d7a5a56f60192b2b493
Lisa Gee <zhusufeng@gmail.com>
2018-03-19 15:42:49 -0700

    Add idea #2 to longestPalindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -19,6 +19,9 @@
 
 /*
   Strategy
+    Constraints
+        Max length of string is 1000
+
     Idea #1 
       Iterate thru string
         Iterate thru remaining string
@@ -29,61 +32,75 @@
       Iterate thru array
         Update longestPalindrome to longestPalindrome found
 
-  Big-O
-    Time: Quadratic
-    Space: Quadratic/Polynomial
+    Idea #1 Big-O
+      Time: Quadratic
+      Space: Quadratic/Polynomial
 
-  Constraints
-    Max length of string is 1000
-
-  Steps
-    Given 'cbbd'
-    i = 0
-      substring = c
-      substrings = [c]
-        j = 1
-        substring = cb
-        substrings = [c, cb]
+    Steps
+      Given 'cbbd'
+      i = 0
+        substring = c
+        substrings = [c]
+          j = 1
+          substring = cb
+          substrings = [c, cb]
+          j = 2
+          substring = cbb
+          substrings = [c, cb, cbb]
+          j = 3 
+          substring = cbbd
+          substrings = [c, cb, cbb, cbbd]
+      i = 1
+        substring = b
+        substrings = [c, cb, cbb, cbbd, b]
         j = 2
-        substring = cbb
-        substrings = [c, cb, cbb]
-        j = 3 
-        substring = cbbd
-        substrings = [c, cb, cbb, cbbd]
-    i = 1
-      substring = b
-      substrings = [c, cb, cbb, cbbd, b]
-      j = 2
-        substring = bb
-        substrings = [c, cb, cbb, cbbd, b, bb]
-      j = 3
-        substring = bbd
-        substrings = [c, cb, cbb, cbbd, b, bb, bbd]
-    i = 2
-      substring = b
-      substrings = [c, cb, cbb, cbbd, b, bb, b]
+          substring = bb
+          substrings = [c, cb, cbb, cbbd, b, bb]
         j = 3
-        substring = bd
-        substrings = [c, cb, cbb, cbb, cbbd, b, bb, b, bd]
-    i = 3
-      substring = d
-      substrings = [c, cb, cbb, cbbd, b, bb, b, bd, d]
+          substring = bbd
+          substrings = [c, cb, cbb, cbbd, b, bb, bbd]
+      i = 2
+        substring = b
+        substrings = [c, cb, cbb, cbbd, b, bb, b]
+          j = 3
+          substring = bd
+          substrings = [c, cb, cbb, cbb, cbbd, b, bb, b, bd]
+      i = 3
+        substring = d
+        substrings = [c, cb, cbb, cbbd, b, bb, b, bd, d]
 
-    i = 0, c === palindrome
-    palindromes = [c]
-    i = 1, cb !== palindrome
-    i = 2, cbb !== palindrome
-    i = 3, cbbd !== palindrome
-    i = 4, b === palindrome
-    palindromes = [c, b]
-    i = 5, bb = palindrome
-    palindromes = [c, b, bb]
-    i = 6, b = palindrome
-    palindromes = [c, b, bb, b]
-    i = 7, bd !== palindrome
-    i = 8, d === palindrome
-    palindromes = [c, b, bb, b, d]
+      i = 0, c === palindrome
+      palindromes = [c]
+      i = 1, cb !== palindrome
+      i = 2, cbb !== palindrome
+      i = 3, cbbd !== palindrome
+      i = 4, b === palindrome
+      palindromes = [c, b]
+      i = 5, bb = palindrome
+      palindromes = [c, b, bb]
+      i = 6, b = palindrome
+      palindromes = [c, b, bb, b]
+      i = 7, bd !== palindrome
+      i = 8, d === palindrome
+      palindromes = [c, b, bb, b, d]
 
+      i = 0, longestPalindrome = c
+      i = 1, longestPalindrome = c
+      i = 2, longestPalindrome = bb
+      i = 3, longestPalindrome = bb
+      i = 4, longestPalindrome = bb
+
+    Idea #2
+      longestPalindrome = ''
+      Iterate thru string
+        Iterate thru remaining string
+          Use helper func isPalindrome to check if string inside is palindrome
+          If it is, check if palindromic substring is longer than longestPalindrome
+            If longer, update longestPalindrome to substring
+
+    Idea #2 Big-O
+      Time: Quadratic
+      Space: Constant
 
   Skeleton  
     const longestPalindrome = s => {
