4e271de356ba938f41dd85ab255a99dd06a9fbe3
Lisa Gee <zhusufeng@gmail.com>
2018-03-20 22:35:04 -0700

    Add strategy, big-O & constraint to anyPermIsPalindrome

--- anyPermIsPalindrome.js
+++ anyPermIsPalindrome.js
@@ -15,3 +15,19 @@ const output2 = false;
 const input3 = 'penguin';
 const output3 = false;
 
+/*
+  Strategy
+    Make a hash map of letters & their count (iterate thru string)
+    Count num of odd occurences (which can only happen once for valid palindrome) (iterate thru hash map)
+
+  Big-O
+    Time: Linear => O(2n)
+    Space: Constant to size of input O(1)
+
+  Constraint
+    Input will only be lowercase letters
+    Efficient!
+
+  Skeleton
+    
+*/
