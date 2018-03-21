ab313e58a5f230b4faad8b0dd81ef1c2ab1836da
Lisa Gee <zhusufeng@gmail.com>
2018-03-20 23:55:09 -0700

    Adjust code with base case/edge case for recursivePerm

--- permutation/recursivePermutation.js
+++ permutation/recursivePermutation.js
@@ -30,16 +30,36 @@ const output1 = new Set(['a', 'ab', 'b', 'ba']);
     };
 */
 
-const makeRecursivePermutations = (s = '', word = '', perms = []) => {
+// const makeRecursivePermutations = (s, word = '', perms = []) => {
+//   if (s.length === 0) {
+//     return [];
+//   }
+//   for (let i = 0; i < s.length; i += 1) {
+//     perms.push(word + s[i]);
+//     makeRecursivePermutations(s.substring(0, i) + s.substring(i + 1), word + s[i], perms);
+//   }
+//   return perms;
+// };
+
+// Set version
+const makeRecursivePermutations = (s, word = '', perms = new Set()) => {
   if (s.length === 0) {
-    return;
+    return new Set();
   }
   for (let i = 0; i < s.length; i += 1) {
-    perms.push(word + s[i]);
+    perms.add(word + s[i]);
     makeRecursivePermutations(s.substring(0, i) + s.substring(i + 1), word + s[i], perms);
   }
   return perms;
 };
 
+console.log(makeRecursivePermutations('a'));
 console.log(makeRecursivePermutations('ab'));
 console.log(makeRecursivePermutations('abc'));
+console.log(makeRecursivePermutations('abcc')); 
+// set version works with duplicate characters
+console.log(makeRecursivePermutations('')); 
+// works with empty string
+
+
+
