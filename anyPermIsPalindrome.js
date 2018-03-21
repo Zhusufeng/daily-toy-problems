d9059de5c84e2a58f3fac1f24c3058daffbc96e9
Lisa Gee <zhusufeng@gmail.com>
2018-03-20 22:30:39 -0700

    Add new yet similar prompt & example inputs/outputs

---
+++ anyPermIsPalindrome.js
@@ -0,0 +1,17 @@
+/*
+  Prompt: https://www.interviewcake.com/question/javascript/permutation-palindrome
+*/
+
+/*
+  Inputs & Outputs
+*/
+
+const input1 = 'aabb';
+const output1 = true;
+
+const input2 = 'livic';
+const output2 = false;
+
+const input3 = 'penguin';
+const output3 = false;
+
