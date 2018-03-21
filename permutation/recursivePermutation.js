45a153330d0b8b8b14f064624736e52f2c6942c2
Lisa Gee <zhusufeng@gmail.com>
2018-03-20 23:36:24 -0700

    Add new prompt w/constraints & example input called recursivePerm

---
+++ permutation/recursivePermutation.js
@@ -0,0 +1,25 @@
+/*
+  Prompt: https://www.interviewcake.com/question/javascript/recursive-string-permutations
+*/
+
+/*
+  Input & Output
+*/
+const input1 = 'ab';
+const output1 = new Set(['a', 'ab', 'b', 'ba']);
+
+
+/*
+  Constraints
+    Must be recursive
+    All chars in string are unique
+
+  Strategy
+
+  Big-O
+
+  Skeleton
+    const makeRecursivePermutations = s => {
+  
+    };
+*/
