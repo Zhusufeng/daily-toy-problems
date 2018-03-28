7a66713c7a3cc7bb2c5df1c116d7c6c01e121b89
Lisa Gee <zhusufeng@gmail.com>
2018-03-27 23:04:38 -0700

    Add new toy problem queueTwoStacks

---
+++ queueTwoStacks.js
@@ -0,0 +1,14 @@
+/*
+  Prompt: https://www.interviewcake.com/question/javascript/queue-two-stacks
+*/
+
+/*
+  Inputs & Outputs
+*/
+
+const oddNums = new Queue();
+oddNums.enqueue(1);
+oddNums.enqueue(2);
+oddNums.enqueue(3);
+oddNums.dequeue();
+// oddNums.storage = 2, 3
