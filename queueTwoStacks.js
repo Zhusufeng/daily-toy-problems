2c4289c03a8409b71f4681105a6b99f5780b1255
Lisa Gee <zhusufeng@gmail.com>
2018-03-31 23:49:06 -0700

    Add comment of example oddNums in queueTwoStacks

--- queueTwoStacks.js
+++ queueTwoStacks.js
@@ -8,7 +8,19 @@
 
 const oddNums = new Queue();
 oddNums.enqueue(1);
+// oddNums.storage = 1
 oddNums.enqueue(2);
+// oddNums.storage = 1, 2
 oddNums.enqueue(3);
+// oddNums.storage = 1, 2, 3
 oddNums.dequeue();
 // oddNums.storage = 2, 3
+
+/*
+  Strategy
+    stack1
+    stack2
+
+  Constraints
+    Constant enqueue and dequeue
+*/
