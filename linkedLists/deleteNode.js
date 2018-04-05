d0851ad991b355f268f7336e3d7f9d373619d9c7
Lisa Gee <zhusufeng@gmail.com>
2018-04-04 23:58:28 -0700

    Add deleteNode code & test if working

--- linkedLists/deleteNode.js
+++ linkedLists/deleteNode.js
@@ -18,18 +18,23 @@ var c = new LinkedListNode('C');
 a.next = b;
 b.next = c;
 
-deleteNode(b);
-
 /*
   Strategy
-    Change b.value to this.next and b.next to this.next.next
+    Change b.value to this.next.value and b.next to this.next.next
 
   Constraints
     You are only given a variable pointing to that node
   
   Skeleton
     const deleteNode = node => {
-    
+      node.value = node.next.value;
+      node.next = node.next.next;
     };
 */
 
+const deleteNode = node => {
+  node.value = node.next.value;
+  node.next = node.next.next;
+};
+deleteNode(b);
+console.log(a); // 'A' -> 'C'
