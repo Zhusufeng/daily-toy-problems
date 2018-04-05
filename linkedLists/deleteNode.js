0cbf4881466c73556b8fc4fd643ae5a28e5f30df
Lisa Gee <zhusufeng@gmail.com>
2018-04-04 23:52:19 -0700

    Add example to deleteNode

--- linkedLists/deleteNode.js
+++ linkedLists/deleteNode.js
@@ -1,3 +1,35 @@
 /*
   Prompt: https://www.interviewcake.com/question/javascript/delete-node
 */
+
+/*
+  Inputs & Outputs
+*/
+
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
+
+var a = new LinkedListNode('A');
+var b = new LinkedListNode('B');
+var c = new LinkedListNode('C');
+
+a.next = b;
+b.next = c;
+
+deleteNode(b);
+
+/*
+  Strategy
+  
+
+  Constraints
+
+  
+  Steps
+
+  Skeleton
+
+*/
+
