68fd9a73be7c12b2c71c0da498d9237ab2bb2967
Lisa Gee <zhusufeng@gmail.com>
2018-04-06 00:05:10 -0700

    Add skeleton for code for linkedListCycles

--- linkedLists/linkedListCycles.js
+++ linkedLists/linkedListCycles.js
@@ -8,15 +8,27 @@
     If fast pointer eqls slow pointer return true
 
   Steps
-    counter = 0, slow = null, fast = a
-    counter = 1, slow = null, fast = b
-    counter = 2, slow = a,    fast = c
-    counter = 3, slow = a,    fast = d
-    counter = 4, slow = b,    fast = c
-    counter = 5, slow = b,    fast = d
-    counter = 6, slow = c,    fast = c
-    return true
- 
+    slow = a, fast = a, no check, counter = 0
+    slow = a, fast = b, a === b?, counter = 1
+    slow = b, fast = c, b === c?, counter = 2
+    slow = b, fast = d, b === d?, counter = 3
+    slow = c, fast = c, c === c?, return true
+
+  Skeleton
+    const containsCycle = (node) => {
+      let counter = 0;
+      let slow = node;
+      let fast = node;
+
+      // while slow is not null
+        // if counter is not 0 and slow.value eqls fast.value
+          // return true
+        // increment counter
+        // update fast to fast.next
+        // if counter is positive
+          // update slow to slow.next
+      return false;
+    };
 */
 
 /*
