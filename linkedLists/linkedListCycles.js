326a1b20ceac3a5f4798176c00f550906dd7b728
Lisa Gee <zhusufeng@gmail.com>
2018-04-05 23:44:39 -0700

    Add example input & output for linkedListCycles

--- linkedLists/linkedListCycles.js
+++ linkedLists/linkedListCycles.js
@@ -3,9 +3,25 @@
 */
 
 /*
-  Inputs & Outputs
+  Strategy
 */
 
 /*
-  Strategy
+  Inputs & Outputs
 */
+
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
+
+const a = new LinkedListNode('a');
+const b = new LinkedListNode('b');
+const c = new LinkedListNode('c');
+const d = new LinkedListNode('d');
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = c;
+
+containsCycle(a); // true
