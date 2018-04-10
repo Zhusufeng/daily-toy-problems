2030b2e755c2c9b2c4adabef3f588d34c8cfd355
Lisa Gee <zhusufeng@gmail.com>
2018-04-09 23:15:11 -0700

    Add an example input & output for reverseLinkedList

--- linkedLists/reverseLinkedList.js
+++ linkedLists/reverseLinkedList.js
@@ -1,3 +1,33 @@
 /*
   Prompt: https://www.interviewcake.com/question/javascript/reverse-linked-list
 */
+
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
+
+/*
+  Inputs & Outputs
+*/
+const input1 = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: null
+    }
+  }
+};
+
+const output1 = {
+  value: 3,
+  next: {
+    value: 2,
+    next: {
+      value: 1,
+      next: null
+    }
+  }
+};
