7835060d4fa2f0c1b91ccde4387ffe63650a23e8
Lisa Gee <zhusufeng@gmail.com>
2018-04-21 20:04:44 -0700

    Create 2nd strategy by making list doubly linked in kthToLastNode

--- linkedLists/kthToLastNode.js
+++ linkedLists/kthToLastNode.js
@@ -25,20 +25,60 @@ d.next = e;
 /*
   Strategy 1
     Iterate thru entire linked list saving into an array, return array[array.length - k]
-
 */
 
+// const kthToLastNode = (k, head) => {
+//   const array = [];
+//   while (head.next) {
+//     array.push(head);
+//     head = head.next;
+//   }
+//   return array[array.length - k];
+// };
+// /*
+//   Time Complexity: Linear
+//   Space Complexity: Constant as saving references in array?
+// */
+
+// console.log(kthToLastNode(2, a)); // Works :)
+
+/*
+  Strategy 2
+    Iterate thru entire linked list & making it a doubly linked list & walk back k nodes
+*/
 const kthToLastNode = (k, head) => {
-  const array = [];
-  while (head.next) {
-    array.push(head);
-    head = head.next;
+  let prev = null;
+  let counter = 1;
+  let last = null;
+  let kthNode = null;
+  while (head !== null) {
+    // add new property to node to make it doubly linked
+    head.prev = prev;
+
+    // save node to be used in the next node
+    prev = head;
+
+    // if you are at last node, save it as last
+    if (head.next === null) {
+      last = head;
+    }
+
+    // go to next node
+    head = head.next    
   }
-  return array[array.length - k];
+  while (counter <= k && last.prev) {
+    if (counter === k) {
+      kthNode = last;
+      break;
+    }
+    counter += 1;
+    last = last.prev;
+  }
+  return kthNode;
 };
 /*
   Time Complexity: Linear
-  Space Complexity: Constant as saving references in array?
+  Space Complexity: Constant
 */
 
-console.log(kthToLastNode(2, a));
+console.log(kthToLastNode(2, a)); 
