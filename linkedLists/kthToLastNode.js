ba24fdcd373406b5fc917e4d76018ca0d84356a5
Lisa Gee <zhusufeng@gmail.com>
2018-04-21 19:17:09 -0700

    Create a brute force method for kthToLastNode

--- linkedLists/kthToLastNode.js
+++ linkedLists/kthToLastNode.js
@@ -19,5 +19,26 @@ b.next = c;
 c.next = d;
 d.next = e;
 
-kthToLastNode(2, a);
+// kthToLastNode(2, a);
 // returns the node with value "Devil's Food" (the 2nd to last node)
+
+/*
+  Strategy 1
+    Iterate thru entire linked list saving into an array, return array[array.length - k]
+
+*/
+
+const kthToLastNode = (k, head) => {
+  const array = [];
+  while (head.next) {
+    array.push(head);
+    head = head.next;
+  }
+  return array[array.length - k];
+};
+/*
+  Time Complexity: Linear
+  Space Complexity: Constant as saving references in array?
+*/
+
+console.log(kthToLastNode(2, a));
