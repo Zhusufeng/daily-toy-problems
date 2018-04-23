c703262ede90830305dcf5b9c8fee08355746662
Lisa Gee <zhusufeng@gmail.com>
2018-04-22 20:51:26 -0700

    Add new prompt reverseInPlace with inputs & outputs

---
+++ arrays/reverseInPlace.js
@@ -0,0 +1,10 @@
+/*
+  Prompt: https://www.interviewcake.com/question/javascript/reverse-string-in-place
+*/
+
+/* Inputs & Outputs */
+const input1 = [1, 2, 3, 4, 5];
+const output1 = [5, 4, 3, 2, 1];
+
+const input2 = ['a', 'b', 'c', 'd'];
+const output2 = ['d', 'c', 'b', 'a'];
