573da351b0df7f8bbb9de1f98a99d5af4b83d446
Lisa Gee <zhusufeng@gmail.com>
2018-06-11 18:52:10 -0700

    Create a 4th strategy, add code & test

--- updateLight.js
+++ updateLight.js
@@ -34,6 +34,11 @@ const output3 = 'green';
       If yellow, return red
       If red, return green
       Else 'Bad input. Choose green, yellow or red'
+
+    #4 Object (or Set)
+      Make object where green is key & yellow is val, etc
+      If obj does not have input as property, return 'Bad input. Choose green, yellow or red'
+      Else return obj[light]
 */
 
 const updateLightArr = (light) => {
@@ -49,9 +54,10 @@ const updateLightArr = (light) => {
   }
 };
 
-console.assert(updateLightArr('green')==='yellow', 'updateLightArr was given green, should return yellow');
-console.assert(updateLightArr('yellow')==='red', 'updateLightArr was given yellow, should return red');
-console.assert(updateLightArr('red')==='green', 'updateLightArr was given red, should return green');
+console.assert(updateLightArr('green') === 'yellow', 'updateLightArr was given green, should return yellow');
+console.assert(updateLightArr('yellow') === 'red', 'updateLightArr was given yellow, should return red');
+console.assert(updateLightArr('red') === 'green', 'updateLightArr was given red, should return green');
+console.assert(updateLightArr('purple') === 'Bad input. Choose green, yellow or red', 'updateLightArr was given red, should return green');
 
 updateLightSwitch = (light) => {
   switch (light) {
@@ -69,6 +75,7 @@ updateLightSwitch = (light) => {
 console.assert(updateLightSwitch('green')==='yellow', 'updateLightSwitch was given green, should return yellow');
 console.assert(updateLightSwitch('yellow')==='red', 'updateLightSwitch was given yellow, should return red');
 console.assert(updateLightSwitch('red')==='green', 'updateLightSwitch was given red, should return green');
+console.assert(updateLightSwitch('purple') === 'Bad input. Choose green, yellow or red', 'updateLightSwitch was given red, should return green');
 
 updateLightIf = light => {
   if (light === 'green') {
@@ -85,3 +92,22 @@ updateLightIf = light => {
 console.assert(updateLightIf('green')==='yellow', 'updateLightIf was given green, should return yellow');
 console.assert(updateLightIf('yellow')==='red', 'updateLightIf was given yellow, should return red');
 console.assert(updateLightIf('red')==='green', 'updateLightIf was given red, should return green');
+console.assert(updateLightIf('purple') === 'Bad input. Choose green, yellow or red', 'updateLightIf was given red, should return green');
+
+updateLightObj = light => {
+  const lights = {
+    green: 'yellow',
+    yellow: 'red',
+    red: 'green'
+  };
+  if (!lights[light]) {
+    return 'Bad input. Choose green, yellow or red';
+  } else {
+    return lights[light];
+  }
+};
+
+console.assert(updateLightObj('green')==='yellow', 'updateLightObj was given green, should return yellow');
+console.assert(updateLightObj('yellow')==='red', 'updateLightObj was given yellow, should return red');
+console.assert(updateLightObj('red')==='green', 'updateLightObj was given red, should return green');
+console.assert(updateLightObj('purple') === 'Bad input. Choose green, yellow or red', 'updateLightObj was given red, should return green');
