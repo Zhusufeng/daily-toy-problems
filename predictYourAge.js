799d0b233252d0f8ab159afbfe3e79bb4964d700
Lisa Gee <zhusufeng@gmail.com>
2018-07-16 18:56:15 -0700

    Add code to predictYourAge

--- predictYourAge.js
+++ predictYourAge.js
@@ -16,3 +16,13 @@
     Use reduce where I sum multiply each number to itself & add to a running total
     Square root the sum & then divide in 2
 */
+
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+  const ages = [...arguments];
+  const sum = ages.reduce((acc, curr) => {
+    return acc + (curr * curr);
+  }, 0);
+  return Math.floor(Math.sqrt(sum) / 2);
+}
+
+console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45));
