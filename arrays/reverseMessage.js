78dee0fc0106253d965dee73317dbccb62ef2285
Lisa Gee <zhusufeng@gmail.com>
2018-08-13 18:48:09 -0700

    Start reverseMessage

---
+++ arrays/reverseMessage.js
@@ -0,0 +1,17 @@
+/*
+  Prompt: https://www.interviewcake.com/question/javascript/reverse-words?section=array-and-string-manipulation&course=fc1
+*/
+
+/*
+  Inputs & Outputs
+*/
+var input1 = [ 'c', 'a', 'k', 'e', ' ',
+                'p', 'o', 'u', 'n', 'd', ' ',
+                's', 't', 'e', 'a', 'l' ];
+var output1 = [ 's', 't', 'e', 'a', 'l', ' ',
+                'p', 'o', 'u', 'n', 'd', ' ',
+                 'c', 'a', 'k', 'e'];
+
+/*
+  Strategy
+*/
